2b87bd00a281f20cada1dd317d14f2f3e4b49844
atealeaf <157325099+atealeaf@users.noreply.github.com>
2025-04-08 23:25:11 -0400

    Add selection bits to top module

--- program_counter.sv
+++ program_counter.sv
@@ -6,43 +6,40 @@ module program_counter (
     input  logic  [2:0]  func3,
     input  logic  [31:0] rs1v,
     input  logic  [31:0] rs2v,
+    input  logic         select_pc_value, // Select PC + 4 or ALU Output
     output logic  [31:0] pc
 );
 
-// Decide select bits
-logic select_bit;
+    // Decide select bits
+    always_ff @(posedge clk) begin
 
-always_ff @(posedge clk) begin
+        if (opcode == 7'b1100011) begin // check for B-type
+            case (func3) // Check which B-type
+                3'b000: // 000 beq =
+                    select_pc_value <= (rs1v == rs2v);
+                3'b001: // 001 bne !=
+                    select_pc_value <= (rs1v != rs2v);
+                3'b100: // 100 blt <
+                    select_pc_value <= (rs1v < rs2v);
+                3'b101: // 101 bge >=
+                    select_pc_value <= (rs1v >= rs2v);
+                3'b110: // 110 bltu < unsigned
+                    select_pc_value <= (rs1v < rs2v);
+                3'b111: // 111 bgeu > unsigned
+                    select_pc_value <= (rs1v >= rs2v);
+            endcase
+        end
 
-    select_bit <= 1'b0;
+        if (opcode == 7'b1100111) begin // check for jalr
+            select_pc_value <= 1'b1;
+        end
 
-    if (opcode == 7'b1100011) begin // check for B-type
-        case (func3) // Check which B-type
-            3'b000: // 000 beq =
-                select_bit <= (rs1v == rs2v);
-            3'b001: // 001 bne !=
-                select_bit <= (rs1v != rs2v);
-            3'b100: // 100 blt <
-                select_bit <= (rs1v < rs2v);
-            3'b101: // 101 bge >=
-                select_bit <= (rs1v >= rs2v);
-            3'b110: // 110 bltu < unsigned
-                select_bit <= (rs1v < rs2v);
-            3'b111: // 111 bgeu > unsigned
-                select_bit <= (rs1v >= rs2v);
+        case (select_pc_value)
+            1'b0: 
+                pc <= pc + 4;
+            1'b1: 
+                pc <= alu_output;
         endcase
     end
-
-    if (opcode == 7'b1100111) begin // check for jalr
-        select_bit <= 1'b1;
-    end
-
-    case (select_bit)
-        1'b0: 
-            pc <= pc + 4;
-        1'b1: 
-            pc <= alu_output;
-    endcase
-end
 
 endmodule
